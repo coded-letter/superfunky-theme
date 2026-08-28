70eb2b13a86dc77f2592c24621c6fb876ba06c3e
George Lazaridis <57189926+Ys-sudo@users.noreply.github.com>
2026-08-28 16:07:20 +0200

    chore: sync funkycommerce-headless from monorepo (#17)
    Source: coded-letter/coded-letter-monorepo@95339e345eae0577fd6ec2a8748b636e645d5150
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'FUNKYCOMMERCE_HEADLESS_VERSION', '1.2.16' );
+define( 'FUNKYCOMMERCE_HEADLESS_VERSION', '1.2.17' );
 
 /**
  * Whether Superfunky Pro is active and licensed.
